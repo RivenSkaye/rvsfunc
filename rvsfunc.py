dd384b50aea695e3517e59b6a5e9939371f349bd
RivenSkaye <pretender0906@gmail.com>
2021-05-04 09:44:02 +0200

    Added useful code

--- rvsfunc.py
+++ rvsfunc.py
@@ -3,13 +3,87 @@ import vsutil
 from typing import Any, Dict, Callable, Optional
 core = vs.core
 
-def nc_splice(source: vs.VideoNode, nc: vs.VideoNode, startframe: int, endframe: int, nc_filterfunc: Optional[Callable[[vs.VideoNode, Any], vs.VideoNode]]=None) -> vs.VideoNode:
+def nc_splice(source: vs.VideoNode, nc: vs.VideoNode, startframe: int, endframe: int,
+              nc_filterfunc: Optional[Callable[[vs.VideoNode, Any], vs.VideoNode]]=None,
+              use_internal: bool=False, ext_mask: Optional[vs.VideoNode]=None, **kwargs) -> vs.VideoNode:
+    """ Function for splicing in video from a different source.
+
+    Originally written to splice in NCs whilst keeping the option open to keep
+    the episode credits, this function takes an optional callable to perform
+    any filtering on the NC. For example merging in the credits from the source
+    clip. Assumes that `nc` will be added into the source clip in full. If this
+    behavior is undesirable, either use a different function for it or send in
+    only the part of the second clip that needs to be spliced in.
+
+    :param source:          The source clip that needs something replaced
+    :param nc:              The clip that needs to be spliced into source
+    :param startframe:      The frame to start splicing at. This is an inclusive
+                            selection value, and in reality 1 is added to it due to
+                            how slicing works in Python. The selected range is
+                            `source[:startframe+1]`.
+    :param endframe:        The first frame that needs to be kept. This is an
+                            inclusive selection value, selected as `source[endframe:]`.
+    :param nc_filterfunc:   The function to call to perform filering on the NC.
+                            For convenience, all additional kwargs will be
+                            passed into this function as well. Use this for any
+                            arguments that the external function needs.
+    :param use_internal:    Whether or not to use internal functions to mask in
+                            the difference between source and nc. As this is
+                            originally meant to merge in credits, the internal
+                            function `copy_credits` which uses some borrowed
+                            logic in an attempt to merge in all credits from
+                            the target range in the source clip. Using the
+                            internal functions or nc_filterfunc are mutually
+                            exclusive. Kwargs will not be passed on to this.
+                            Defaults to `False`
+    :param ext_mask:        For when the internal merging function is good enough
+                            but the mask it generates isn't. This will only be
+                            used if `use_internal` is set to `True` and will
+                            be passed on for use in `copy_credits`
+    """
     if nc_filterfunc:
-        nc = nc_filterfunc(nc)
+        nc = nc_filterfunc(nc, **kwargs)
+    elif use_internal:
+        nc = copy_credits(source[startframe:endframe+1], nc, ext_mask)
     out = source[:startframe+1] + nc + source[endframe:]
     return out
 
+def copy_credits(source: vs.VideoNode, nc: vs.VideoNode, mask: Optional[vs.VideoNode]=None) -> vs.VideoNode:
+    """ Copy credits from the source to the nc using a diff_mask.
+
+    This function assumes there is no notable difference between the NC and the
+    source clip so that the internal mask is gonna catch nothing but
+    credits and perhaps a few stray pixels.
+    Returns the NC with the credits merged in from the source.
+
+    :param source:      The clip to take the credits from. If you need them from
+                        a subsection of the full clip, slice it yourself.
+    :param nc:          The NC to copy the credits into
+    :param mask:        Optionally an external mask to use for copying over the
+                        credits if the detail_mask doesn't do a good enough job.
+    """
+    def _mask(clipa, clipb):
+        clipa = core.resize.Point(clipa, format=clipb.format.id) if not clipa.format.id == clipb.format.id else clipa
+        return core.std.Expr([sy, ry], 'x y - abs').std.Binarize()
+    mask = _mask(source,nc) if not mask else mask
+    return core.std.MaskedMerge(nc, source, mask)
+
 def detail_mask(source: vs.VideoNode, rescaled: vs.VideoNode, thresh: float=0.05) -> vs.VideoNode:
+    """ Generates a fairly basic detail mask
+
+    This was originally part of `questionable_descale`'s internal logic, but
+    the detail mask can be used in more places as well. The mask is pretty coarse
+    and grabs a bit more than it'd strictly need, but you can mess with the thresh
+    if you need it to pick up more or less.
+    This is mostly used to pick up on detail _lost_ in `questionable_descale` as
+    per Zastin's original script. That said, the mask serves other typical uses
+    as well. For example grabbing credits and native different res elements in
+    the source clip.
+
+    :param source:      The clip to generate the mask for.
+    :param rescaled:    The descaled and re-upscaled clip where detail was lost.
+    :param thresh:      The threshold for binarizing the detail mask
+    """
     sy = vsutil.get_y(source)
     ry = vsutil.get_y(rescaled)
     sy = core.resize.Point(sy, format=ry.format.id) if not sy.format.id == ry.format.id else sy
