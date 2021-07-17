3475a5599a231ec2d9d66bd373cee2188a0c6cfe
RivenSkaye <pretender0906@gmail.com>
2021-07-17 13:20:52 +0200

    Added a function to index stuff in batches

--- rvsfunc.py
+++ rvsfunc.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Callable, Optional
+from typing import Any, Dict, Callable, Optional, Union, List
 from math import floor
 from functools import partial
 
@@ -6,6 +6,34 @@ import numpy as np
 import vsutil
 import vapoursynth as vs
 core = vs.core
+
+def batchindex(paths: Union[List[str],str], source_filter: Callable[[str], Any],
+               show_list: bool=False) -> List[Any]:
+    """ Index sources in batch, provide a list of files to index.
+
+    Simple lazy function. Takes a path or a list of paths, indexes them and
+    then frees the memory again before returning on success. If you happen
+    to get any errors or exceptions, this will just raise the same thing again.
+
+    :param paths:           A single path as a string or a List of paths.
+    :param source_filter:   The source filter or indexer method to call. If it
+                            doesn't write, make sure to get the list of indexes
+                            using show_list.
+    :param show_list:       If this is set to True, this function returns the
+                            results of `source_filter(path)` for every path in
+                            paths. Might be useful for batches as well as it
+                            would just return the `vs.VideoNode`s returned by
+                            the calls to source_filter.
+    """
+    paths = [paths] if isinstance(paths, str) else paths
+    sauces = []
+    try:
+        for p in paths:
+            sauces.append(source_filter(p))
+        if not show_list: del sauces
+    except Error as e:
+        raise e
+    return [] if not show_list else sauces
 
 def nc_splice(source: vs.VideoNode, nc: vs.VideoNode, startframe: int, endframe: int,
               nc_filterfunc: Optional[Callable[[vs.VideoNode, Any], vs.VideoNode]]=None,
