ebd9df32461033eab9afabc809fcd3876d44ada7
RivenSkaye <pretender0906@gmail.com>
2021-09-14 10:55:39 +0200

    Create stubs for rvsfunc

---
+++ mypy-stubs/rvsfunc/__init__.pyi
@@ -0,0 +1,4 @@
+from . import cursed, dvd as dvd, masking as masking, utils
+
+batchindex = utils.batch_index
+voodoo = cursed
---
+++ mypy-stubs/rvsfunc/dvd.pyi
@@ -0,0 +1,7 @@
+import vapoursynth as vs
+from typing import Any, Callable, Dict
+
+core: Any
+
+def eoe_convolution(clip: vs.VideoNode): ...
+def chromashifter(clip: vs.VideoNode, wthresh: int = ..., vertical: bool = ..., maskfunc: Callable[[vs.VideoNode, Any], vs.VideoNode] = ..., mask_kwargs: Dict = ..., shifter: Callable[[vs.VideoNode, Any], vs.VideoNode] = ...) -> vs.VideoNode: ...
