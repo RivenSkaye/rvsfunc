57ea5ceab0febdad7ba4c38832cba7eabacf59a5
RivenSkaye <pretender0906@gmail.com>
2021-09-14 13:16:50 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as rq:
 
 setup(
     name="rvsfunc",
-    version="0.0.4",
+    version="0.0.5",
     author="Riven Skaye",
     author_email="riven@tae.moe",
     description="VapourSynth functions written or modified by Riven Skaye",
