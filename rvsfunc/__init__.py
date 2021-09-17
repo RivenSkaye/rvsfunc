81e555b881e7927800630e7c389441f7f7264692
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2021-09-17 17:55:13 +0200

    Format __init__.py

--- rvsfunc/__init__.py
+++ rvsfunc/__init__.py
@@ -1,4 +1,5 @@
-""" A collection of VapourSynth stuff I wrote or mangled to make life easier.
+"""
+A collection of VapourSynth stuff I wrote or mangled to make life easier.
 
 Most of these were because I'm too lazy for my own good, some out of
 desperation and edgecases.
@@ -8,8 +9,8 @@ trying to keep sane defaults so minimum use should produce nice results.
 Good luck figuring out which functions you need, docs came soon™!
 
 If you spot any issues, you can find me on Discord as @Riven Skaye#0042,
-or contribute by `sending in a PR <https://github.com/RivenSkaye/rvsfunc/blob/master/CONTRIBUTING.md>`_!
-"""  # noqa: E501 W505
+or contribute by `sending in a PR <https://github.com/RivenSkaye/rvsfunc/blob/master/CONTRIBUTING.md>`!
+"""
 
 from . import utils, masking, dvd, cursed  # noqa: F401
 
