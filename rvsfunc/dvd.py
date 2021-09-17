92be25e1a10069108893aabfa24753fdf207ad98
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2021-09-17 17:40:29 +0200

    Format dvd.py

--- rvsfunc/dvd.py
+++ rvsfunc/dvd.py
@@ -1,5 +1,4 @@
 """ Functions written specifically for issues common or exclusive to DVDs.
-
 DVDs, ancient as the specs and carriers are, come with their fair share of
 typical issues. Some of the more well-known problems are things like getting
 telecined before editing, chroma shifts that change from scene to scene and
@@ -8,28 +7,37 @@ by several subcontractors. They're generally a pain in the ass and this
 module exists to alleviate some of that pain.
 """
 
+import numpy as np
+from math import floor
+import vapoursynth as vs
 from .masking import eoe_convolution
 from typing import Callable, Dict, Any
-from math import floor
-import numpy as np
-import vapoursynth as vs
+
+
 core = vs.core
 
 
-def chromashifter(clip: vs.VideoNode, wthresh: int = 31, vertical: bool = False,
-                  maskfunc: Callable[[vs.VideoNode], vs.VideoNode] = eoe_convolution,  # noqa: E501
-                  mask_kwargs: Dict = {},
-                  shifter: Callable[[vs.VideoNode, Any], vs.VideoNode] = core.resize.Point  # noqa: E501
-                  ) -> vs.VideoNode:
-    """ Automatically fixes chroma shifts, at the very least by approximation.
+def frame_to_array(frame: vs.VideoFrame) -> np.ndarray:
+    frame_array = []
+    for plane in range(frame.format.num_planes):
+        plane_array = np.array(frame.get_read_array(plane), copy=False)
+        frame_array.append(plane_array.reshape(list(plane_array.shape) + [1]))
+    return np.concatenate(frame_array, axis=2)
 
+
+def chromashifter(
+    clip: vs.VideoNode, wthresh: int = 31, vertical: bool = False,
+    maskfunc: Callable[[vs.VideoNode], vs.VideoNode] = eoe_convolution,
+    mask_kwargs: Dict = {},
+    shifter: Callable[[vs.VideoNode, Any], vs.VideoNode] = core.resize.Point
+) -> vs.VideoNode:
+    """ Automatically fixes chroma shifts, at the very least by approximation.
     This function takes in a clip and scales it to a 4x larger YUV444P clip.
     It then generates edgemasks over all of the planes to be used in distance
     calculations to figure out the proper value to shift chroma with.
     The shift is applied to the otherwise untouched input clip.
     Assumes the shift is the same for the U and V planes.
     Actually fast now thanks to EoE :eoehead:
-
     :param clip: vs.VideoNode:  The clip to process. This may take a while.
     :param wthresh: int:        The threshold for white values to use in the
                                 calculations for proper shifting.
@@ -51,23 +59,26 @@ def chromashifter(clip: vs.VideoNode, wthresh: int = 31, vertical: bool = False,
                                 requirements to prevent errors.
     :return: vs.VideoNode:      The input clip, but without chroma shift.
     """
-    _fname = "rvsfunc chromashifter:"
-    if not clip.format:
-        raise vs.Error(f"{_fname} The clip must have a constant format. \
-                        Please convert or split the clip.")
-    if clip.format.color_family is not vs.YUV:
-        raise vs.Error(f"{_fname} The clip MUST be of the YUV color family. \
-                       Please convert it before calling this function.")
-    if clip.format.num_planes < 3:
-        raise vs.Error(f"{_fname} This function requires all three planes in \
-                       order to calculate proper shifts.")
 
-    def frame_to_array(frame: vs.VideoFrame) -> np.ndarray:
-        frame_array = []
-        for plane in range(frame.format.num_planes):
-            plane_array = np.array(frame.get_read_array(plane), copy=False)
-            frame_array.append(plane_array.reshape(list(plane_array.shape) + [1]))  # noqa: E501
-        return np.concatenate(frame_array, axis=2)
+    _fname = "rvsfunc chromashifter:"
+
+    if not clip.format:
+        raise vs.Error(
+            f"{_fname} The clip must have a constant format. "
+            "Please convert or split the clip."
+        )
+
+    if clip.format.color_family is not vs.YUV:
+        raise vs.Error(
+            f"{_fname} The clip MUST be of the YUV color family. "
+            "Please convert it before calling this function."
+        )
+
+    if clip.format.num_planes < 3:
+        raise vs.Error(
+            f"{_fname} This function requires all three planes in "
+            "order to calculate proper shifts."
+        )
 
     shifted_clips: Dict[float, vs.VideoNode] = {0: clip}
 
@@ -81,40 +92,46 @@ def chromashifter(clip: vs.VideoNode, wthresh: int = 31, vertical: bool = False,
                 continue
             try:  # some edgecase frames produce 0 divisions
                 if (
-                    array_above[row, row_first[row, 1], 1] and
-                    row_first[row, 1] <= row_first[row, 2]
+                    array_above[row, row_first[row, 1], 1] and row_first[row, 1] <= row_first[row, 2]
                 ):
                     shifts.append(
-                        256 / round((wthresh + 1) * (((row_first[row, 1] - luma_col) + 1) / 8))  # noqa: E501
+                        256 / round((wthresh + 1) * (((row_first[row, 1] - luma_col) + 1) / 8))
                     )
                     continue
                 if array_above[row, row_first[row, 2], 2]:
                     shifts.append(
-                        256 / round((wthresh + 1) * (((row_first[row, 2] - luma_col) + 1) / 8))  # noqa: E501
+                        256 / round((wthresh + 1) * (((row_first[row, 2] - luma_col) + 1) / 8))
                     )
             except ZeroDivisionError:
                 continue
+
         try:
             shift = sum(shifts) / len(shifts)
             if shift > 2 or shift < -2:
                 shift = shift - (floor(shift) / 2)
         except ZeroDivisionError:
             shift = 0
+
         shift = round(shift * 8) / 8
         shifted = shifted_clips.get(shift)
+
         if shifted is None:
-            shifted = shifter(clip, src_left=shift)  # type: ignore
+            shifted = shifter(clip, src_left=shift)
             shifted_clips.update({shift: shifted})
+
         return shifted
 
     if vertical:
         clip = core.std.Transpose(clip)
-    yuv = clip.resize.Spline36(height=clip.height * 4, width=clip.width * 4,
-                               format=vs.YUV444P8)
+
+    yuv = clip.resize.Spline36(height=clip.height * 4, width=clip.width * 4, format=vs.YUV444P8)
+
     if maskfunc is core.std.Prewitt:
         mask_kwargs["planes"] = [0, 1, 2]
-    yuv = maskfunc(yuv, **mask_kwargs)  # type: ignore
+
+    yuv = maskfunc(yuv, **mask_kwargs)
 
     out = core.std.FrameEval(clip, get_shifted, yuv)
     out = core.std.ShufflePlanes([clip, out], [0, 1, 2], vs.YUV)
+
     return out.std.Transpose() if vertical else out
