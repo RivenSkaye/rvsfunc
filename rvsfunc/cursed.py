abf0a5b3204243f7657d720e61074b1e84d5b948
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2021-09-17 18:29:51 +0200

    Max 85 Columns, Format, Fix Bugs

--- rvsfunc/cursed.py
+++ rvsfunc/cursed.py
@@ -1,15 +1,16 @@
-""" Edgecase functions that were generalized for if I ever need them again.
+"""
+Edgecase functions that were generalized for if I ever need them again.
 Most of these functions should not be used in the general use case. They're
 typically written for a very specific edgecase, but generalized for if I ever
 need them again. Currently only holds `questionable_rescale` which I mangled
 from someone else's code that had a very similar edgecase.
 """
 
-import vsutil
 from math import log
 import vapoursynth as vs
 from .masking import detail_mask
 from typing import Any, Dict, Callable, Optional
+from vsutil import depth, split, join, get_depth, get_w, get_y
 
 
 core = vs.core
@@ -22,7 +23,8 @@ def nnedi3_rpow2(
     nsize: int = 0, nns: int = 3,
     **kwargs: Dict[str, Any]
 ) -> vs.VideoNode:
-    """ nnedi3_rpow2, increasing frame dimensions by powers of 2 using NNEDI3.
+    """
+    nnedi3_rpow2, increasing frame dimensions by powers of 2 using NNEDI3.
     Aims to replace the old nnedi3_rpow2 by being a resolvable dependency and
     not providing most of the standard NNEDI args due to them remaining default.
     Lists non-default arguments and adds an optional argument to enable use
@@ -57,7 +59,8 @@ def nnedi3_rpow2(
     # Check the clip format to prevent issues and bullshittery.
     if clip.format is not None and clip.format.color_family not in [vs.GRAY, vs.YUV]:
         raise vs.Error(
-            "rpow2 only takes constant format YUV clips, please fix your input and try again."
+            "rpow2 only takes constant format YUV clips, "
+            "please fix your input and try again."
         )
 
     # Check if the rfactor is within the legal ranges
@@ -101,33 +104,50 @@ def nnedi3_rpow2(
         args: Dict
     ) -> vs.VideoNode:
         doubled = nn(vid, field=0, **args)
-        if fix:
-            shiftval = 0.5 + (0.25 - (vid.width / (vid.width * 2))) if vid.width == src_width / 2 else 0.5
-            if opencl:
-                shift_v = 0.5 + (0.25 - (vid.height / (vid.height * 2))) if vid.width == src_width / 2 else 0.0
-            else:
-                shift_v = 0.0
-            return doubled.resize.Spline36(src_left=shiftval, src_top=shift_v)
-        return doubled
+
+        if not fix:
+          return doubled
+
+        shift_v, shift_h = 0.0, 0.5
+
+        if vid.width == src_width / 2:
+            shift_h += 0.25 - (vid.width / (vid.width * 2))
+
+        if opencl and vid.width == src_width / 2:
+            shift_v = 0.5 + (0.25 - (vid.height / (vid.height * 2)))
+
+        return doubled.resize.Spline36(src_left=shift_h, src_top=shift_v)
 
     # Calulate how many times we have to run this thing
     count = log(rfactor, 2)
 
     for _ in range(count):
-        planes = vsutil.split(clip)
-        shifted = [_double(p, clip.width, nnedi, shift, cl, nnedi_kwargs) for p in planes]
+        planes = split(clip)
+
+        shifted = [
+          _double(p, clip.width, nnedi, shift, cl, nnedi_kwargs) for p in planes
+        ]
+
         if not cl:  # Handle width if we're not using NNEDI3CL for it
             shifted = [s.std.Transpose() for s in shifted]
-            shifted = [_double(s, clip.width, nnedi, shift, cl, nnedi_kwargs) for s in shifted]
+            shifted = [
+              _double(s, clip.width, nnedi, shift, cl, nnedi_kwargs) for s in shifted
+            ]
             shifted = [s.std.Transpose() for s in shifted]
-        clip = vsutil.join(shifted)
-    return vsutil.depth(clip, depth_in, sample_type=sample_in)
+
+        clip = join(shifted)
+
+    return depth(clip, depth_in, sample_type=sample_in)
+
+
+_Descaler = Callable[[vs.VideoNode, int, int, Any], vs.VideoNode]
+_Scaler = Callable[[vs.VideoNode, int, int], vs.VideoNode]
 
 
 def questionable_rescale(
     clip: vs.VideoNode, height: int, b: float = 1 / 3, c: float = 1 / 3,
-    descaler: Callable[[vs.VideoNode, int, int, Any], vs.VideoNode] = core.descale.Debicubic,
-    scaler: Optional[Callable[[vs.VideoNode, int, int], vs.VideoNode]] = core.resize.Spline36,
+    descaler: _Descaler = core.descale.Debicubic,
+    scaler: Optional[_Scaler] = core.resize.Spline36,
     scale_kwargs: Dict = {"height": None}, correct_shift: bool = True,
     apply_mask: bool = True, mask_thresh: float = 0.05,
     ext_mask: Optional[vs.VideoNode] = None, depth_out: int = -1,
@@ -161,7 +181,7 @@ def questionable_rescale(
     :param ext_mask:        Supply your own mask instead of the default.
     :param depth_out:       The output depth. Values below 0 will cause it to
                             use the depth of the input clip, any other values
-                            will be passed to ``vsutil.depth`` the way they are
+                            will be passed to ``depth`` the way they are
                             with no regards to what may be raised.
     :param return_mask:     Whether to return the mask used instead of the clip.
                             This requires ``scaler`` to not be None.
@@ -180,47 +200,60 @@ def questionable_rescale(
     if scale_kwargs.get("height") is None:
         scale_kwargs["height"] = clip.height
 
-    depth_out = vsutil.get_depth(clip) if depth_out < 0 else depth_out
+    if depth_out < 0:
+        depth_out = get_depth(clip)
 
-    if vsutil.get_depth(clip) > 16 or clip.format.sample_type == vs.FLOAT:
-        clip = vsutil.depth(clip, 16, sample_type=vs.INTEGER)
+    if get_depth(clip) > 16 or clip.format.sample_type == vs.FLOAT:
+        clip = depth(clip, 16, sample_type=vs.INTEGER)
 
     rgv = core.rgvs.RemoveGrain(clip, mode=1)
-    clamp = vsutil.depth(rgv, 32, dither_type="none")
-    clip = vsutil.depth(clip, 32, dither_type="none")
+
+    clamp, clip = depth(rgv, 32), depth(clip, 32)
+
     chroma = clip.format.num_planes > 1
 
     if chroma:
-        y, u, v = vsutil.split(clip)
-        cy, cu, cv = vsutil.split(clamp)
+        y, cy = get_y(clip), get_y(clamp)
     else:
-        y = clip
-        cy = clamp
+        y, cy = clip, clamp
 
-    descy = descaler(y, width=vsutil.get_w(height, clip.width / clip.height), height=height, b=b, c=c)
-    desccy = descaler(cy, width=vsutil.get_w(height, clip.width / clip.height), height=height, b=b, c=c)
+    descy = descaler(y, get_w(height, clip.width / clip.height), height, b, c)
+    desccy = descaler(cy, get_w(height, clip.width / clip.height), height, b, c)
 
-    err = descy.resize.Bicubic(clip.width, clip.height, filter_param_a=b, filter_param_b=c)
-    diff_a = core.std.Expr([y, err], "x y - abs")
-    cerr = desccy.resize.Bicubic(clip.width, clip.height, filter_param_a=b, filter_param_b=c)
-    diff_b = core.std.Expr([cy, cerr], "x y - abs")
-    pre_descale = core.std.Expr([diff_a, diff_b, y, cy], f"x y - {1000/(1<<16)-1} > x {2500/(1<<16)-1} > and z a ?")
+    def _get_err_diff(y: vs.VideoNode) -> vs.VideoNode:
+      err = y.resize.Bicubic(y.width, y.height, filter_param_a=b, filter_param_b=c)
+      return core.std.Expr([y, err], "x y - abs")
 
-    descaled = descaler(pre_descale, width=vsutil.get_w(height, clip.width / clip.height), height=height, b=b, c=c)
+    diff_a, diff_b = _get_err_diff(descy), _get_err_diff(desccy)
+
+    peak = (1 << 16) - 1
+
+    pre_descale = core.std.Expr(
+      [diff_a, diff_b, y, cy],
+      f"x y - {1000 / peak} > x {2500 / peak} > and z a ?"
+    )
+
+    descaled = descaler(
+      pre_descale, get_w(height, clip.width / clip.height), height, b, c
+    )
 
     if not scaler:
         return descaled
 
-    doubled = nnedi3_rpow2(descaled, shift=correct_shift, cl=True)
-    doubled = scaler(doubled, **scale_kwargs)
+    doubled = scaler(
+      nnedi3_rpow2(descaled, shift=correct_shift, cl=True), **scale_kwargs
+    )
 
     if apply_mask:
-        mask = detail_mask(y, doubled, thresh=mask_thresh) if not ext_mask else ext_mask
+        if not ext_mask:
+          mask = detail_mask(y, doubled, thresh=mask_thresh)
+        else:
+          mask = depth(ext_mask, get_depth(doubled))
         if return_mask:
             return mask
         doubled = core.std.MaskedMerge(doubled, y, mask)
 
     if chroma:
-        doubled = vsutil.join([doubled, u, v])
+        doubled = core.std.ShufflePlanes([doubled, clip], [0, 1, 2], vs.YUV)
 
-    return vsutil.depth(doubled, depth_out)
+    return depth(doubled, depth_out)
