965e3809565bc31c8e5f4d83625ef271213a5872
RivenSkaye <pretender0906@gmail.com>
2021-09-28 09:44:21 +0200

    Yeet old stubs, generate new ones soon

--- mypy-stubs/rvsfunc/__init__.pyi
+++
@@ -1,4 +0,0 @@
-from . import cursed, dvd as dvd, masking as masking, utils
-
-batchindex = utils.batch_index
-voodoo = cursed
--- mypy-stubs/rvsfunc/cursed.pyi
+++
@@ -1,7 +0,0 @@
-import vapoursynth as vs
-from .masking import detail_mask as detail_mask
-from typing import Any, Callable, Dict, Optional
-
-core: Any
-
-def questionable_rescale(clip: vs.VideoNode, height: int, b: float = ..., c: float = ..., descaler: Callable[[vs.VideoNode, Any], vs.VideoNode] = ..., scaler: Callable[[vs.VideoNode, Any], vs.VideoNode] = ..., scale_kwargs: Dict = ..., correct_shift: bool = ..., apply_mask: bool = ..., mask_thresh: float = ..., ext_mask: Optional[vs.VideoNode] = ..., depth_out: int = ..., return_mask: bool = ...) -> vs.VideoNode: ...
