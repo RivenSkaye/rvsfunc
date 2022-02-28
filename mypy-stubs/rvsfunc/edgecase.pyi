7e0df085085757abbd6bbbe0259de618ccc86be1
Riven Skaye <pretender0906@gmail.com>
2022-02-28 17:17:33 +0100

    Yeet old stubs. I'll make new ones later

--- mypy-stubs/rvsfunc/edgecase.pyi
+++
@@ -1,9 +0,0 @@
-import vapoursynth as vs
-from .NNEDI3 import ZNEDI3 as ZNEDI3
-from .masking import detail_mask as detail_mask
-from typing import Any, Callable, Dict, Optional
-
-core: Any
-
-def chunked_filter(clip: vs.VideoNode, func: Callable[[vs.VideoNode], vs.VideoNode], *, hchunks: int = ..., vchunks: int = ...) -> vs.VideoNode: ...
-def questionable_rescale(clip: vs.VideoNode, height: int, b: float = ..., c: float = ..., descaler: _Descaler = ..., scaler: Optional[_Scaler] = ..., scale_kwargs: Dict = ..., correct_shift: bool = ..., apply_mask: bool = ..., mask_thresh: float = ..., ext_mask: Optional[vs.VideoNode] = ..., depth_out: int = ..., return_mask: bool = ...) -> vs.VideoNode: ...
