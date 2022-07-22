bf66084a4ac4c4ac4bd7f517ec22b3e76ca3e6e7
Riven Skaye <pretender0906@gmail.com>
2022-07-22 13:25:06 +0200

    Defining this on init makes stuff break

--- rvsfunc/utils.py
+++ rvsfunc/utils.py
@@ -19,10 +19,9 @@ from .masking import detail_mask
 
 
 core = vs.core
-vs_api_below4: bool = vs.__api_version__.api_major < 4  # type: ignore [attr-defined]
 
 __all__ = [
-    "vs_api_below4", "is_topleft", "batch_index", "replace", "replace_ranges",
+    "is_topleft", "batch_index", "replace", "replace_ranges",
     "copy_credits", "frame_to_array", "pad_to"
 ]
 
@@ -196,8 +195,8 @@ def frame_to_array(f: vs.VideoFrame) -> np.ndarray:
     Simple wrapper to turn a video frame into an numpy array
     """
     return np.dstack([
-        f.get_read_array(p) for p in range(f.format.num_planes)  # type: ignore
-    ] if vs_api_below4 else f)
+        f.get_read_array(p) for p in range(f.format.num_planes)
+    ] if vs.__api_version__.api_major < 4 else f)  # type: ignore
 
 
 def pad_to(
